05707cf0f2247bddc268210b55afec027c5a1cae
FrancescaFazio <86601546+FrancescaFazio@users.noreply.github.com>
2022-08-26 16:26:02 +0200

    Add files via upload

---
+++ App_test/gui_test/src/main/java/org/fazioMonchieri/models/Seggio.java
@@ -0,0 +1,39 @@
+package org.fazioMonchieri.models;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Seggio {
+    private Integer id;
+    private String via;
+    private String CAP;
+    private String citta;
+    private String provincia;
+    
+    public Seggio(){
+        
+    }
+    
+    public Integer getId(){
+        return id;
+    }
+    
+    public String getVia(){
+        return via;
+    }
+    
+    public String getCAP(){
+        return CAP;
+    }
+    
+    public String getCitta(){
+        return citta;
+    }
+    
+    public String getProvincia(){
+        return provincia;
+    }
+}
