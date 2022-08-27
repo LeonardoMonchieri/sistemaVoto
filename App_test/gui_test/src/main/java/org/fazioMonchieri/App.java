96acb8a2e1db3c02fba7affb7fcda462b12f993c
Leonardo Monchieri <80100758+LeonardoMonchieri@users.noreply.github.com>
2022-08-27 12:54:12 +0200

    App upload

--- App_test/gui_test/src/main/java/org/fazioMonchieri/App.java
+++ App_test/gui_test/src/main/java/org/fazioMonchieri/App.java
@@ -1,41 +1,38 @@
 package org.fazioMonchieri;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.io.File;
+
 import java.io.IOException;
-import java.util.*;
+
 
 import org.fazioMonchieri.utilities.Controller;
-//import org.fazioMonchieri.data.DbManager;*/
 
 public class App extends Application {
-    private static Scene scene;
 
-    public static boolean homeLoaded;
+    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
         //DbManager db = DbManager.getInstance();
-
         //db.ensureCreated();
-
-        scene = new Scene(loadView("HomeView"), 1280, 720);
+        scene = new Scene(loadView("HomeView"), 870, 600);
         navigate(null, "HomeView");
         stage.setScene(scene);
         stage.setTitle("Voting system");
         stage.setMinWidth(900);
         stage.setMinHeight(600);
         stage.show();
-        homeLoaded = true;
     }
 
     public static Parent loadView(String view) throws IOException {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("views/" + view + ".fxml"));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("/views/" + view + ".fxml"));
         return loader.load();
     }
 
@@ -44,7 +41,7 @@ public class App extends Application {
     }
 
     public static Parent loadView(Controller sender, String view, Object parameter) throws IOException {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("views/" + view + ".fxml"));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("/views/" + view + ".fxml"));
         Parent parent = loader.load();
         Controller controller = loader.getController();
         controller.onNavigateFrom(sender, parameter);
@@ -60,8 +57,9 @@ public class App extends Application {
     public static void navigate(Controller sender, String view) throws IOException {
         navigate(sender, view, null);
     }
-
+ 
     public static void main(String[] args) {
         launch();
     }
+    
 }
