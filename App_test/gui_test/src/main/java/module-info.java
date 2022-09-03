e455c08172483554700c37942302bfcbe6876036
Leonardo Monchieri <80100758+LeonardoMonchieri@users.noreply.github.com>
2022-09-03 15:41:23 +0200

    Update module-info.java

--- App_test/gui_test/src/main/java/module-info.java
+++ App_test/gui_test/src/main/java/module-info.java
@@ -1,20 +1,18 @@
 module org.openjfx {
     requires javafx.controls;
-    requires ormlite.core;
-    requires ormlite.jdbc;
     requires java.sql;
     requires sqlite.jdbc;
     requires javafx.base;
     requires javafx.fxml;
 
     exports org.fazioMonchieri;
-    //exports org.fazioMonchieri.models;
-    //exports org.fazioMonchieri.data;
+    exports org.fazioMonchieri.models;
+    exports org.fazioMonchieri.data;
     exports org.fazioMonchieri.controllers;
 
 
-    //opens org.fazioMonchieri.models;
-    //opens org.fazioMonchieri.data;
+    opens org.fazioMonchieri.models;
+    opens org.fazioMonchieri.data;
     opens org.fazioMonchieri.controllers;
     
 }
